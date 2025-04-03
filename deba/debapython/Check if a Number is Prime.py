c409138a27e04ee03c31d229fdb44a3621be9cc6
deba-tech-hub <debaprasadgouda238@gmail.com>
2025-04-04 00:25:53 +0530

    5 python code task

---
+++ deba/debapython/Check if a Number is Prime.py
@@ -0,0 +1,14 @@
+//Check if a Number is Prime
+
+
+
+num = int(input("Enter a number: "))  
+if num > 1:  
+    for i in range(2, int(num ** 0.5) + 1):  
+        if num % i == 0:  
+            print("Not Prime")  
+            break  
+    else:  
+        print("Prime")  
+else:  
+    print("Not Prime")
