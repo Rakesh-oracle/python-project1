743790f03167f144df3f95583238c951301c5e57
Paresh Kumar Sahu <pareshsahu100@gmail.com>
2025-04-08 14:36:18 +0530

    FirstClass

---
+++ paresh/javaparesh/FirstClass.java
@@ -0,0 +1,12 @@
+class FirstClass {
+public static void main(String args[])
+{
+System.out.println("Hello World");
+if(3 % 2==0){
+System.out.println("Bozinga");
+}
+else{
+    System.out.println("Not Bozinga");
+}
+}
+}
