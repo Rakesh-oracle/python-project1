b4460251bdbf61497f22e4a84a0e24b243daa7ec
RajaLabs04 <shibarath2002@gmail.com>
2025-04-11 15:31:49 +0530

    surprise

---
+++ Alisha/grade.py
@@ -0,0 +1,14 @@
+grade = input("Enter a grade (O,A,B,C,F):")
+ 
+if grade == 'O':
+  print("Outstanding")
+elif grade == 'A':
+  print("Very Good")
+elif grade == 'B':
+  print("Good")
+elif grade == 'C':
+  print("Average")
+elif grade == 'F':
+  print("Fail")
+else:
+  print("Invalid grade entered.Please enter one of the following: O,A,B,C,F.")
