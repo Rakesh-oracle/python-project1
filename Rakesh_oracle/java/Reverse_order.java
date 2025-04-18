d169f68897e99e39f9fe3c4d3b7c1910816e78fc
Rakesh-oracle <rakeshpatro7653@gmail.com>
2025-04-19 02:20:19 +0530

    New programs

---
+++ Rakesh_oracle/java/Reverse_order.java
@@ -0,0 +1,7 @@
+public class Reverse_order {
+    public static void main(String[] args) {
+        for(int i=20;i>=0;i--){
+            System.out.println("Reverse order Numbers: "+ i);
+        }
+    }
+}
---
+++ Rakesh_oracle/java/odd_number.java
@@ -0,0 +1,8 @@
+public class odd_number {
+    public static void main(String[] args){
+
+        for(int i = 0; i<5; i++) {
+            System.out.println( "odd numbers:"+ (2 * i + 1));
+        }
+    }
+}
