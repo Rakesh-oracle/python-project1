ab868e8881de9ec35116cc71bdc689fbddf80888
Rakesh-8520 <sahurakeshkumar469@gmail.com>
2025-06-12 23:17:26 +0530

    Revert "Dev1"

--- rak2rak/javaprgm/Current.java
+++
@@ -1,8 +0,0 @@
-public class Current {
-    public static void main(String[] args) {
-        System.out.println("Let us find current thread");
-        Thread t = Thread.currentThread();
-        System.out.println("Current Thread ="+t);
-        System.out.println("The name of the Thread is :"+t.getName());
-}
-}
--- rak2rak/javaprgm/myframe.java
+++
@@ -1,8 +0,0 @@
-import java.awt.*;
-public class myframe {
-    public static void main(String[] args) {
-        Frame f=new Frame("my awt frame");
-        f.setSize(1300,1250);
-        f.setVisible(true);
-    }
-}
